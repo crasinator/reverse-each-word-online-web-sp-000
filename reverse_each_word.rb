91cc4d7e78c787a4d8dd3d9c7c497e4015810f3e
Jordan Reed <jordanreed03@gmail.com>
2020-11-30 19:46:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
 string = string.split(" ")
-  string.collect { |love|
-     var = love.reverse
+  string.each { |love|
+    love.reverse
   }
-  string = string
-  string.join(" ")
+ 
 end
